e77dae4c57fecac1f9b71460f197654c924f33fe
nonoumasy <nonoumasy@gmail.com>
2020-12-30 04:50:31 -0800

    asfasd

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlay,
