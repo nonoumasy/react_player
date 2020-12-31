5729434ab3de84a1257cacb225997c323bee4560
nonoumasy <nonoumasy@gmail.com>
2020-12-30 22:07:12 -0800

    songs correct

--- src/util.js
+++ src/util.js
@@ -6,7 +6,7 @@ function chillHop() {
       cover:
         'https://www.rhino.com/sites/rhino.com/files/styles/article_image/public/2018-07/081227993702.jpg?itok=BfHQJWQh',
       artist: 'Jesus Mary Chain',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=10075',
+      audio: 'https://media.hungama.com/c/4/9bc/ae7/47847818/47847818_128.mp3?Kv4z0qcCyQQ9tizY-tNUOd-4j6IYYEL982bdsWPP8nDR6ZFPTa1CtINubKXlQ3RAhg3jUwzmFxdCW3IzVkeUWdo91vRknFnr0On9sVx44Nx74k1kZyESndulKbPat44Y4yDMVg',
       color: ['#205950', '#2ab3bf'],
       id: uuidv4(),
       active: true,
@@ -63,7 +63,7 @@ function chillHop() {
       cover:
         'https://www.rhino.com/sites/rhino.com/files/styles/square/public/products/darklands.jpg?itok=tUqWxuBP',
       artist: 'Jesus Mary Chain',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9222',
+      audio: 'https://media.hungama.com/c/4/14a/7e0/3463076/3463076_128.mp3?_OvuQspIVs7Vwr3QyE_TIby5Qh11ViyILN8HbKx4WZZ8ZyRpQNW098pKKtdzxak0CTXLjXFCYFTlOXNt9HdVkQr-zxMplZbOV-0w7eIEz0cJfZOOpBGhRphrT405yjHDTWo',
       color: ['#CD607D', '#c94043'],
       id: uuidv4(),
       active: false,
