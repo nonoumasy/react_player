e3518c378b203aebb2d2f8f5407d60b218e17a24
nonoumasy <nonoumasy@gmail.com>
2021-01-01 06:02:29 -0800

    implemented drag n drop

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import LibrarySong from './LibrarySong';
+// import LibrarySong from './LibrarySong';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Library = ({
   songs,
@@ -13,17 +14,30 @@ const Library = ({
   setIsLibOpen,
 }) => {
 
-  const renderSongItems = () => {
-    return songs.map((song) => (
-      <LibrarySong
-        key={song.id}
-        song={song}
-        currentSong={currentSong}
-        setCurrentSong={setCurrentSong}
-        setIsPlaying={setIsPlaying}
-      />
-    ));
+  const [items, setItems] = useState([])
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
   };
+  
+  const onDragEnd = (result) =>  {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+  const items = reorder(
+    songs,
+    result.source.index,
+    result.destination.index
+  );
+
+    setItems(items);
+  }
 
   return (
     <div className={`library ${isLibOpen ? 'active-library' : ''}`}>
@@ -33,9 +47,58 @@ const Library = ({
           <FontAwesomeIcon icon={faTimes} size="1x" />
         </button>
       </div>
-      <div className="library-songs">
-        {renderSongItems()}
-      </div>
+
+      {/* <div className="library-songs">
+        {songs.map((song) => (
+          <div
+            className={`library-song ${song.id === currentSong.id ? 'active' : ''}`}
+            onClick={() => setCurrentSong(song)}
+          >
+            <img src={song.cover} alt={song.name} />
+            <div className="song-description">
+            <h3>{song.name}</h3>
+            <h4>{song.artist}</h4>
+            </div>
+          </div>
+        ))}
+      </div> */}
+
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {songs.map((song, index) => (
+                <Draggable key={song.id} draggableId={song.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <div
+                        className={`library-song ${song.id === currentSong.id ? 'active' : ''}`}
+                        onClick={() => setCurrentSong(song)}
+                      >
+                        <img src={song.cover} alt={song.name} />
+                        <div className="song-description">
+                        <h3>{song.name}</h3>
+                        <h4>{song.artist}</h4>
+                        </div>
+                      </div>
+
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+        
       
     </div>
   );
