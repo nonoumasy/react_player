fc993c1aa2c644fecfc29e70eed0be9b54e10d70
nonoumasy <nonoumasy@gmail.com>
2021-01-01 00:23:37 -0800

    dfsadf

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,7 +4,7 @@ const Song = ({ currentSong, albumRef, isPlaying}) => {
 
   return (
     <div className="song-container">
-      {isPlaying ? <img className="album_coverRotating" src={currentSong.cover} alt={currentSong.name} /> : <img className="album_cover" ref={albumRef} src={currentSong.cover} alt={currentSong.name} />}
+      {isPlaying ?< img className="album_coverActive"  src={currentSong.cover} alt={currentSong.name} /> : <img className="album_cover" ref={albumRef} src={currentSong.cover} alt={currentSong.name} />}
       
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
