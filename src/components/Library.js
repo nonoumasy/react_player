a8477812c3760a84f36b75009ee35e96f36f2481
nonoumasy <nonoumasy@gmail.com>
2021-01-06 04:33:02 -0800

    fixed equalizer

--- src/components/Library.js
+++ src/components/Library.js
@@ -74,7 +74,7 @@ const Library = ({
                           onClick={() => clickHandler(song)}
                         >
                           <div>
-                            {isPlaying && <img src={eqGif} alt='' className={`eqGif ${song.id === currentSong.id ? 'active' : ''}`} />}
+                            {isPlaying && <img src={eqGif} alt='' className={`eqGif${song.id === currentSong.id ? 'Active' : ''}`} />}
                             
                             <img src={song.cover} alt={song.name} className={`albumCover ${song.id === currentSong.id ? 'active' : ''}`} />
                           </div>
